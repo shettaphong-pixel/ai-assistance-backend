4946e1616444d841d327f0770e7880ad36dc4f7c
shettaphong-pixel <shettaphong@gmail.com>
2026-09-08 21:24:25 +0700

    server.js

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ async function callGroq(prompt) {
       },
       body: JSON.stringify(
         {
-        model: "llama-3.3-70b-versatile",
+        model: "openai/gpt-oss-120b",
         messages: [
           {
             role: "user",
