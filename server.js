9b67122ecdefb0b3481518a22686573619e274a2
shettaphong-pixel <shettaphong@gmail.com>
2026-09-08 10:04:57 +0700

    server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import cors from "cors";
 import { createClient } from "@supabase/supabase-js";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 
 async function getManuals() {
